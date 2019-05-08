d5407d4dd32eb70f2c1fa93ab774a675f99b88c7
vadim <banzabit@gmail.com>
2019-05-07 23:57:31 -0300

    Add `Welcome and Home screen support

--- gogogo/App.js
+++ gogogo/App.js
@@ -1,49 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, {PureComponent} from 'react';
+import {View, Image} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import reducers from './src/reducers';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import HomeScreen from './src/components/HomeScreen';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            showHome: false,
+        }
+    }
+    componentDidMount() {
+        this.timeoutId = setTimeout(function () {
+            this.setState({showHome: true});
+        }.bind(this), 1000);
+    } 
+
+    componentWillUnmount() {
+        if (this.timeoutId) {
+            clearTimeout(this.timeoutId);
+        }
+    }
+
+    render() {
+        const {showHome} = this.state;
+
+        let enterScreen = (
+            <Image
+                style={styles.image}
+                source={require('./src/image/go1_2x.png')}
+            />
+        );
+
+        if (showHome) {
+            enterScreen = <HomeScreen />;
+        }
+        
+        return (
+            <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+                {enterScreen}
+            </Provider>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = {
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+};
---
+++ gogogo/src/components/HomeScreen.js
@@ -0,0 +1,20 @@
+import React, {PureComponent} from 'react';
+import {Text, View} from 'react-native';
+
+export default class HomeScreen extends PureComponent {
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         showHome: false,
+    //     }
+    // }
+    
+    render() {
+        return (
+            <View>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
